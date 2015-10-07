a9403ccd2031d7d646f7b93614dbdc528879bbb1
Antonio Marques Jr <antonioricardojr@gmail.com>
2015-10-06 22:11:03 -0300

    atualizando tratamento de entrada nos campos do cadastro

--- cadastro-angular/script.js
+++ cadastro-angular/script.js
@@ -1,8 +1,22 @@
-var app = angular.module('minmax', [
+var app = angular.module('cadastro', [
 	'jcs-autoValidate',
-	'angular-ladda'
+	'angular-ladda',
+	'validation.match'
 ]);
 
+app.run(function (defaultErrorMessageResolver) {
+		defaultErrorMessageResolver.getErrorMessages().then(function (errorMessages) {
+			errorMessages['invalidField'] = 'Campo inválido.';
+			errorMessages['emptyField'] = 'Preencha o campo acima.';
+			errorMessages['invalidEmail'] = 'Email inválido.';
+			errorMessages['emailMatchErr'] = 'Email não confere.';
+			errorMessages['nameMatchErr'] = 'Nome não confere.';
+			errorMessages['invalidPassword'] = 'Senha inválida. A senha deve conter no mínimo 10 digitos e ser formada por letras e/ou números.';
+			errorMessages['passwordMatchErr'] = 'Senha nao confere.'
+			errorMessages['textMatchErr'] = 'Texto nao confere.'
+		});
+	}
+);
 
 
 
