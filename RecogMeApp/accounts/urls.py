2e7fb35f443f1e59f68a123f03021b5ab6845290
Leonardo Alves dos Santos <leonardo.santos@ccc.ufcg.edu.br>
2015-10-21 13:49:52 -0300

    Logout

--- RecogMeApp/accounts/urls.py
+++ RecogMeApp/accounts/urls.py
@@ -4,8 +4,9 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-				url(r'^$', views.index_view, name='accounts.index'),
-				url(r'^login/$', views.login_view, name='accounts.login'),
-                url(r'^registro/$', views.registro_usuario_view, name='accounts.registro'),
-                url(r'obrigado/(?P<username>[\w]+)/$', views.obrigado_view, name='accounts.obrigado'),
-                ]
+    url(r'^$', views.index_view, name='accounts.index'),
+    url(r'^login/$', views.login_view, name='accounts.login'),
+    url(r'^logout/$', views.logout_view, name='accounts.logout'),
+    url(r'^registro/$', views.registro_usuario_view, name='accounts.registro'),
+    url(r'obrigado/(?P<username>[\w]+)/$', views.obrigado_view, name='accounts.obrigado'),
+]
