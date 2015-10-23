e36ca8518bf8e0aa7737680946b7b42fc3c60326
Antonio Marques Jr <antonioricardojr@gmail.com>
2015-10-23 13:58:00 -0300

    refatoramento em keystroke.js. Coletando os ids dos inputs que se encontram dentro do form da pagina.

--- cadastro-angular/keystroke.js
+++ cadastro-angular/keystroke.js
@@ -1,4 +1,3 @@
-  
 var Key = function (){
         var keyCode, keyValue, keyDown, keyUp;
     }
@@ -14,58 +13,67 @@ function onKeyDown(field, timestamps){
         
         key = new Key;
         key.keyCode = event.keyCode;
-        key.keyValue = String.fromCharCode(event.keyCode); //get the Char value for a keycode.
+        key.keyValue = String.fromCharCode(event.keyCode); //get the Char value for the keycode.
 
         key.keyDown = event.timeStamp;
         onKeyUp(field, timestamps, key); // keyup listener
         
-        //console.log(timestamps);
         });
     }
     
 function onKeyUp(field, timestamps, k) {
         $( '#' + field ).keyup(function( event ) {
-            if(k.keyCode === event.keyCode){ //check if the keyup event is for the right key
-                if (k.keyUp === undefined) {
+            //check if the keyup event is for the right key
+            if(k.keyCode === event.keyCode){
+
+                //assure if the keyup value for the right key is not defined already
+                if (k.keyUp === undefined) { 
                     k.keyUp = event.timeStamp;
                      
                      k = keyValueCheck(k); //check if the keyValue is empty.
-
                     timestamps.push(k);
+                    console.log(timestamps);
                 }
             }
         });
      }
 
-
-     //check if the keyValue is empty. Return the key object
-     function keyValueCheck (k) {
+//check if the keyValue is empty. Return the key object
+function keyValueCheck (k) {
         if(keycodes[k.keyCode]){
             k.keyValue = keycodes[k.keyCode];
         }
         return k;
      }
 
-//defines an object for the fields in the form. 
-fields = {'fullName': [], 
-          'confirmFullName': [],
-          'email': [], 
-          'confirmEmail': [], 
-          'password': [], 
-          'confirmPassword': [], 
-          'userText': []
-      }
+//defines an object for the fields in the form. Return the IDs for the form fields.
+var fieldNames = function () {
+    var formFields = $("input").not("#PreventAutocomplete"); // get the input fields.
+    var userText = $("textArea")[0].id; //get the id for the text Area 
+    var fieldIDs = {};
+
+    fieldIDs[userText] = []; // add the userText id.
+
+  for (var i = 0; i < formFields.length; i++) {
+    
+    var fieldID = formFields[i].id;
+
+    fieldIDs[fieldID] = [];
+  };
+  return fieldIDs;
+}
+
+var fields = fieldNames();
 
 //applying the keystroke listener for each field in the form.
 for (var field in fields) {
     keystrokeOnField(field, fields[field]);
 }
 
-
 $('#btnclick').click(function (event) {    
     var jsonFields = JSON.stringify(fields);
     console.log(jsonFields);
-    console.log(fields);
+    
 });
 
 //Check the id for the submit button in Django project.
