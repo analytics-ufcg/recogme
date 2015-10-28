2b18d4eab0c0d0bcb50948596416343fba9d2b21
Leonardo Alves dos Santos <leonardo.santos@ccc.ufcg.edu.br>
2015-10-28 15:26:26 -0300

    Merged

--- RecogMeApp/RecogMe/settings.py
+++ RecogMeApp/RecogMe/settings.py
@@ -79,23 +79,23 @@ WSGI_APPLICATION = 'RecogMe.wsgi.application'
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
 # sqlite
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
-# mysql
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'recogme',
-        'USER': 'root',
-        'PASSWORD': '123456',
-        'HOST': '',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+# mysql
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'recogme',
+#         'USER': 'root',
+#         'PASSWORD': '123456',
+#         'HOST': '',
+#         'PORT': '',
+#     }
+# }
 
 
 
