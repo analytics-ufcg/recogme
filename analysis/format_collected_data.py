c35fa5b45a82d33f8c92d8cc23064b59f17dbf76
Tales Tenorio Pimentel <tales.tsp@gmail.com>
2015-11-03 17:14:34 -0300

    [BUG FIX] Character enconding bug fixed.

--- analysis/format_collected_data.py
+++ analysis/format_collected_data.py
@@ -1,8 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import codecs
 import ast
 import json
 import sys
 
+def formated_string_to_dicts_tuple(string):
+	return ast.literal_eval("{" + string + "}")
+
 path = sys.argv[1]
+
+sep = "\t"
 
 arq_user_login = open(path, "r")
 
@@ -12,7 +21,7 @@ lines_user_login = arq_user_login.readlines()
 
 formated_table = []
 
-table_header = ",".join(("attempt_id", "email", "attempt_time", "source", "keyDown", "keyUp", "keyValue", "keyCode"))
+table_header = sep.join(("attempt_id", "email", "attempt_time", "source", "keyDown", "keyUp", "keyValue", "keyCode"))
 formated_table.append(table_header)
 
 print "Processing data"
@@ -24,34 +33,34 @@ for line in lines_user_login:
 	time = line_split[2]
 
 	json_email = line_split[3]
-	email_keystroke = json.loads(json_email)["login"]
+	email_keystroke = formated_string_to_dicts_tuple(json_email.replace("\n","").split("[{")[1].split("}]")[0])
 
 	json_password = line_split[4]
-	password_keystroke = json.loads(json_password)["password"]
+	password_keystroke = formated_string_to_dicts_tuple(json_password.replace("\n","").split("[{")[1].split("}]")[0])
 
 	json_user_text = line_split[5]
-	user_text_keystroke = json.loads(json_user_text)["userText"]
+	user_text_keystroke = formated_string_to_dicts_tuple(json_user_text.replace("\n","").split("[{")[1].split("}]")[0])
 
 	for k_email in email_keystroke:
 		row = (attempt_id, email, time, "email", str(k_email["keyDown"]), str(k_email["keyUp"]), k_email["keyValue"], str(k_email["keyCode"]))
-		row = ",".join(row)
+		row = sep.join(row)
 		formated_table.append(row)
 
 	for k_password in password_keystroke:
 		row = (attempt_id, email, time, "password", str(k_password["keyDown"]), str(k_password["keyUp"]), k_password["keyValue"], str(k_password["keyCode"]))
-		row = ",".join(row)
+		row = sep.join(row)
 		formated_table.append(row)
 
 	for k_text in user_text_keystroke:
 		row = (attempt_id, email, time, "userText", str(k_text["keyDown"]), str(k_text["keyUp"]), k_text["keyValue"], str(k_text["keyCode"]))
-		row = ",".join(row)
+		row = sep.join(row)
 		formated_table.append(row)
+
 
 print "Saving table"
 arq = open(path.replace(".psv", "-formated.psv"), "w")
 for row in formated_table:
 	arq.write(row.encode("utf8") + "\n")
-
 arq.close()
 
 print "Done!"
