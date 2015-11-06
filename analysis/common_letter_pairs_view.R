708aee971e77e1f58c62155694d21b46d6b86d1a
Tales Tenorio Pimentel <tales.tsp@gmail.com>
2015-11-06 15:27:26 -0300

    Script para ver como são os tempos de HOLD das teclas no pares-de-teclas mais pressionados

--- analysis/common_letter_pairs_view.R
+++ analysis/common_letter_pairs_view.R
@@ -2,4 +2,17 @@ occurrence_table <- read.table("~/development/recogme/dados_coletados/Dados_CSV/
 occurrence_table = occurrence_table[with(occurrence_table, order(-occurrence)), ]
 row.names(occurrence_table) = NULL
 
-data = read.table("~/development/recogme/dados_coletados/Dados_CSV/userLogin-1.0.tsv", header=TRUE, sep = "\t", quote = "")
+occurrence = read.table("~/development/recogme/dados_coletados/Dados_CSV/occurrence_table.csv", header=TRUE, sep = "\t", quote = "")
+pairs.of.letter = read.table("~/development/recogme/dados_coletados/Dados_CSV/letter_pairs_by_attempt.tsv", header=TRUE, sep = "\t", quote = "")
+pairs.of.letter$hold = pairs.of.letter$keyUp - pairs.of.letter$keyDown
+
+occurrence = occurrence[with(occurrence, order(-occurrence)), ]
+row.names(occurrence) = NULL
+
+#maior ocorrencia
+a = "A"
+r = "R"
+occurr1.data = pairs.of.letter[pairs.of.letter$key_1st == a & pairs.of.letter$key_2nd == r,]
+
+
+
